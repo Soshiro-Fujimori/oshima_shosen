034cdabbde949b461bdde9b3f5ae00643e6f9ecc
i22110 <oshima.k.kondate@gmail.com>
2023-01-29 13:54:21 +0900

    Update a.c
    added a full name comment and deleted   an unused var

--- 1年/授業/2次元配列/a.c
+++ 1年/授業/2次元配列/a.c
@@ -1,3 +1,4 @@
+// 藤本宗太郎
 // 2次元配列の実習だから2次元配列を使ったけど、
 // 2次元配列使うのナンセンスでは？
 
@@ -7,9 +8,6 @@ int main(void)
 {
     // twoSubPoint[生徒番号][科目番号]
     int twoSubPoint[6][2] = {0};
-    // ttlAndAvrg2[生徒番号][合計:0, 平均:1];
-    int ttlAndAvrg2[6][2] = {0};
-
 
     puts("6人の国語・数学の点数を入力してください。");
     for (int stdntNum = 0; stdntNum < 6; stdntNum++)
@@ -20,8 +18,7 @@ int main(void)
             scanf("%d", &twoSubPoint[stdntNum][subjctNum]);
         }
     }
-    printf("\n");
-    
+    printf("\n");    
 
     puts("科目ごとの合計点と平均点");
     for (int subjctNum = 0; subjctNum < 2; subjctNum++) 
@@ -40,7 +37,6 @@ int main(void)
         if(subjctNum == 1) printf("数学：合計%3d点　平均点%3d点\n", total, avrg);
     }
     printf("\n");
-
 
     puts("学生ごとの合計点と平均点");
     for (int stdntNum = 0; stdntNum < 6; stdntNum++) 
