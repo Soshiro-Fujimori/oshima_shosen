07e8979ac4afd92f52dd28e134fc8e61a5b6b5e7
shiro <oshima.k.kondate@gmail.com>
2023-02-04 17:38:43 +0900

    ho

--- 1年/テスト対策/学年末/2dArray_2.c
+++ 1年/テスト対策/学年末/2dArray_2.c
@@ -2,5 +2,28 @@
 
 int main(void)
 {
+    int array[3][4] = {0};
     for(int arrayNum = 0; arrayNum < 3; arrayNum++)
+    {
+        printf("%d個目の配列\n", arrayNum);
+        for(int elmntNum = 0; elmntNum < 4; elmntNum++)
+        {
+            printf("array[%d][%d]=", arrayNum, elmntNum);
+            scanf("%d", &array[arrayNum][elmntNum]);
+        }
+    }
+
+    for(int arrayNum = 0; arrayNum < 3; arrayNum++)
+    {
+        printf("%d個目の配列\n", arrayNum);
+        int total = 0;
+        for(int elmntNum = 0; elmntNum < 4; elmntNum++)
+        {
+            int element = array[arrayNum][elmntNum];
+            printf("%d ", element);
+            total += element;
+        }
+        printf("\n");
+        printf("合計：%d\n", total);
+    }
 }
